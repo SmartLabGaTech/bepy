7ecaea100e45c8c53ae06acc910eac645a650a38
lgriffin39 <lgriffin39@gatech.edu>
2019-06-21 09:14:38 -0400

    Changed sample load. Now loading a sample will search all folders in the path looking for fit files.

--- bepy/bepy/sample.py
+++ bepy/bepy/sample.py
@@ -28,7 +28,7 @@ class Sample:
         if path is not None:
             self.addentiresample(path, gridSize, adjustphase)
 
-    def addmeasurement(self, meas, measType=None, gridSize=None, adjustphase=True):
+    def addmeasurement(self, meas, measType=None, measName=None, gridSize=None, adjustphase=True):
 
         if measType is None:
             measType = meas._measurementName
@@ -42,32 +42,45 @@ class Sample:
                 raise ValueError(measType+': Unknown measurement type')
 
         else:
-            if measType in ['Vert', 'Lat']:
+            if measType.lower() in 'scan':
                 try:
-                    newMeas = LineMeasurement(meas, adjustphase=adjustphase)
+                    newMeas = LineMeasurement(meas, name=measName, adjustphase=adjustphase)
                 except ValueError:
                     raise ValueError('Argument is not a path to measurement data. Maybe its a Measurement Object?')
                 meas_flags = newMeas.clean()
-                self._linemeasurements[measType] = newMeas
-            elif measType in ['SSPFM', 'NonLin', 'Relax']:
+                self._linemeasurements[measName] = newMeas
+            else:
+                if measType.lower() in ['grid']:
+                    measType = 'Relax'
                 try:
                     newMeas = GridMeasurement(meas, measType, gridSize, adjustphase=adjustphase)
                 except ValueError:
                     raise ValueError('Argument is not a path to measurement data. Maybe its a Measurement Object?')
                 meas_flags = newMeas.clean()
-                self._gridmeasurements[measType] = newMeas
-            else:
-                raise ValueError(measType+': Unknown measurement type')
+                self._gridmeasurements[measName] = newMeas
 
         self._meas_acq_flags[measType] = meas_flags
 
     def addentiresample(self, path, gridSize=50, adjustphase=True):
 
-        foldernames={'BELine': 'Vert', 'BEScan': 'Vert', 'BELat': 'Lat', 'Relax': 'Relax', 'SSPFM': 'SSPFM', 'NonLin': 'NonLin'}
+        directory_list = list()
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in dirs:
+                directory_list.append(os.path.join(root, name))
 
-        for name in foldernames:
-            if os.path.isdir(path+name):
-                self.addmeasurement(path+name+'/', measType=foldernames[name], gridSize=gridSize, adjustphase=adjustphase)
+        for directory in directory_list:
+
+            if os.path.isfile(directory+'/shofit.csv'):
+                parameters = pd.read_csv(directory + '/parameters.csv', header=None, index_col=0)
+
+                try:
+                    measType = parameters.T['Measurement Type'].values[0]
+                    measName = parameters.T['Measurement Name'].values[0]
+                except KeyError:
+                    measType = 'Grid'
+                    measName = 'Fail'
+
+            self.addmeasurement(directory+'/', measType=measType, measName=measName, gridSize=gridSize, adjustphase=adjustphase)
 
     def GetMeasStack(self, measstack = None, varstack = ['Amp', 'Phase', 'Res', 'Q'], inout=0.0, plotGroup=None,
                      insert=None, clean=False):
